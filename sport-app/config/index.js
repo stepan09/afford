b9c39341930a0e4c57da436c47bf35a5ba9dc917
stepan <stepanoliinyk09@gmail.com>
2018-06-01 22:36:28 +0300

    added server.js

--- sport-app/config/index.js
+++ sport-app/config/index.js
@@ -38,10 +38,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../target/dist/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../target/dist'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
